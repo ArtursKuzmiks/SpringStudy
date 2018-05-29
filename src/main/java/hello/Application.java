bbbf14a5faa193fa2a8cbcf2133cba1e28ce4f13
Arturs.Kuzmiks <arturs.kuzmiks@gmail.com>
2018-05-29 14:13:31 +0300

    test

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -13,9 +13,11 @@ public class Application {
     }
 
     public static void main(String[] args) {
+
         ApplicationContext context =
                 new AnnotationConfigApplicationContext(Application.class);
         MessagePrinter printer = context.getBean(MessagePrinter.class);
+
         printer.printMessage();
     }
 }
