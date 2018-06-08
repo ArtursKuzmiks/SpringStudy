3c59933a41e6215ecb9669ea4b60dfffe3c7dfa9
Artur Kuzmik <arturs.kuzmiks@gmail.com>
2018-06-08 12:05:30 +0300

    new Module add

---
+++ SpringData(hibernate)/src/main/java/SqlTest/AppConfig/AppConfig.java
@@ -0,0 +1,77 @@
+package SqlTest.AppConfig;
+
+import SqlTest.Service.Customer;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+/**
+ * @author Artur Kuzmik on 18.29.5
+ */
+
+@Configuration
+@ComponentScan(basePackages = "SqlTest")
+@EnableJpaRepositories(basePackages = "SqlTest")
+@PropertySource(value = {"classpath:app.properties"})
+public class AppConfig {
+
+    private Environment env;
+
+    @Autowired
+    public AppConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getRequiredProperty("jdbc.driverClassName"));
+        dataSource.setUrl(env.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(env.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(env.getRequiredProperty("jdbc.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean emFactory = new LocalContainerEntityManagerFactoryBean();
+        emFactory.setDataSource(dataSource());
+        emFactory.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        emFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        Properties jpaProp = new Properties();
+        jpaProp.setProperty("hibernate.dialect","com.enigmabridge.hibernate.dialect.SQLiteDialect");
+        jpaProp.setProperty("hibernate.hbm2ddl.auto","none");
+        emFactory.setJpaProperties(jpaProp);
+        emFactory.setPackagesToScan("SqlTest");
+
+        return emFactory;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+        return transactionManager;
+    }
+
+    @Bean
+    public Customer customer(){
+        return new Customer();
+    }
+
+}
---
+++ SpringData(hibernate)/src/main/java/SqlTest/Service/Customer.java
@@ -0,0 +1,97 @@
+package SqlTest.Service;
+
+import javax.persistence.*;
+
+/**
+ * @author Artur Kuzmik on 18.29.5
+ */
+
+@Entity
+@Table(name = "md_2DB")
+public class Customer {
+
+    private @Id @GeneratedValue Long ID;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Surname")
+    private String surname;
+
+    @Column(name = "orderDate")
+    private String orderDate;
+
+    @Column(name = "cost")
+    private double cost;
+
+    @Column(name="paid")
+    private double paid;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String surname, String orderDate,
+                    double cost, double paid) {
+        super();
+        this.name = name;
+        this.surname = surname;
+        this.orderDate = orderDate;
+        this.cost = cost;
+        this.paid = paid;
+
+    }
+
+    Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    String getSurname() {
+        return surname;
+    }
+
+    void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    String getOrderDate() {
+        return orderDate;
+    }
+
+    void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    double getCost() {
+        return cost;
+    }
+
+    void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    double getPaid() {
+        return paid;
+    }
+
+    void setPaid(double paid) {
+        this.paid = paid;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-5s\t%-15s\t%-15s\t%-15s\t%.2f\t%.2f",
+                ID, name, surname,orderDate,cost, paid);
+    }
+}
